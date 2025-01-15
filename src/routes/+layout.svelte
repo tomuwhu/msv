ae27c541233b121b2de6a9ff2220afc19c492bd7
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-15 18:52:41 +0100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,46 @@
-<a href="/">Home</a>
-<a href="/demo/lucia">Lucia</a>
-<a href="/cica">Cica</a>
+<script>
+    import { page } from '$app/stores';
+    import { afterUpdate } from 'svelte';
+    let url = $page.url.pathname
+    afterUpdate(() => {
+        url = $page.url.pathname 
+    })
+    const menu = [
+        {
+            name: 'Home',
+            href: '/'
+        },
+        {
+            name: 'Lucia',
+            href: '/demo/lucia'
+        },
+        {
+            name: 'Cica',
+            href: '/cica'
+        }
+    ]
+</script>
+
+{#each menu as item}
+    <a class={item.href === url ? 'active' : ''} href={item.href}>{item.name} ({item.href})</a>
+{/each}
 <hr>
 <slot/>
+<hr>
+<style>
+    a {
+        all: unset;
+        margin: 5px;
+        padding: 5px;
+        border-radius: 5px;
+        cursor: pointer;
+        color: salmon;
+    }
+    a:hover {
+        color: hotpink;
+    }
+    a.active {
+        background-color: hotpink;
+        color: white;
+    }
+</style>
