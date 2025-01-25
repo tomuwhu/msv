5975ddeedcf49283fa5c8f8b83f0abf69f3a8f39
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-25 11:01:38 +0100

    Get Form Data

--- src/routes/form/+page.server.js
+++ src/routes/form/+page.server.js
@@ -1,5 +1,15 @@
+var x = 1
 export const load = () => {
     return {
-        cica: 1
+        cica: x
     }
+}
+export const actions = {
+	default: async ({ locals, request }) => {
+		console.log(locals)
+        const data = await request.formData()
+		const x1 = data.get('x1')
+        console.log(x1)
+        
+	}
 }
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -3,3 +3,8 @@
 </script>
 <h1>Ez lesz a form</h1>
 {data.cica}
+<form method="post">
+    <input name="x1">
+    <br>
+    <input type="submit" value="Elküld">
+</form>
