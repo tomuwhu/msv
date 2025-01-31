c1f0b666d34967166cf6349714869bfbd2f92d1e
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-31 11:23:14 +0100

    Links in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,35 @@
 <h1>Welcome to SvelteKit - Példa projekt (Hunglish version)</h1>
 <div>
     <ul>
-        <li>Drizzle (MySQL)
+        <li><a href="https://kit.svelte.dev">SvelteKit</a>
+            <ul>
+                <li><a target="_blank" href="https://kit.svelte.dev/docs/routing"> Basic Routing / Layout Menu</a></li>
+                <li><a target="_blank"  href="https://svelte.dev/docs/kit/load">Load</a>, <a href="https://svelte.dev/docs/kit/form-actions">Form actions</a></li>
+                <li><a target="_blank"  href="https://svelte.dev/docs/kit/state-management">State management</a></li>
+            </ul>
+        </li>
+        <li><a target="_blank"  href="https://orm.drizzle.team/docs/get-started/mysql-new">Drizzle (MySQL)</a>
             <ul>
                 <li>ORM Schema</li>
+                <li>Query Builder</li>
+                <li>Push</li>
                 <li>CRUD</li>
             </ul>
         </li>
-        <li>Lucia (Login / Registration)</li>
+        <li><a target="_blank"  href="https://lucia-auth.com/sessions/basic-api/drizzle-orm">Lucia (Login / Registration)</a></li>
     </ul>
 </div>
 <style>
     div, h1 {
         font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    }
+    a {
+        all: unset;
+        cursor: pointer;
+        color: rgb(70, 159, 164);
+    }
+    a:hover {
+        color: rgb(178, 95, 74);
     }
     ul {
         margin: auto;
