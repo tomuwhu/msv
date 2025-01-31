36eb0cee771f30759155dcba60d7df11dc55dc67
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-31 10:20:15 +0100

    cica tényleg

--- src/routes/cica/+page.svelte
+++ src/routes/cica/+page.svelte
@@ -11,11 +11,14 @@
     </span>)
 </h1>
 
+{#if data.user}
+    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRLM_YMOn41npXKC5fX-TSRfe20jO-nK1cfON36eskj5100UzlH4JMmJVsjNYxZPV4R0vw6DHIw0dqN-osUB5Iw7Q" alt="cica">
+{/if}
 <style>
 h1 {
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-    margin-bottom: 5px;
-    margin-top: 0px;
+    margin-bottom: 15px;
+    margin-top: 5px;
 }
 span {
     color: rgb(70, 159, 164);
@@ -24,4 +27,10 @@ span.err {
     color: red;
     font-size: 25px;
 }
+img {
+    width: 600px;
+    border-radius: 25px;
+    box-shadow: 4px 4px 14px rgb(58, 16, 16);
+}
+
 </style>
