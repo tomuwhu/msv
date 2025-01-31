8eae53936312bfaf2d8b4ef4967e74350236e6df
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-31 13:57:32 +0100

    MEOW

--- src/routes/cica/+page.svelte
+++ src/routes/cica/+page.svelte
@@ -28,6 +28,9 @@
             })
         })
         sound = await x.json();
+        setTimeout(() => {
+           sound = {} 
+        }, 1000);
     }} 
     src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRLM_YMOn41npXKC5fX-TSRfe20jO-nK1cfON36eskj5100UzlH4JMmJVsjNYxZPV4R0vw6DHIw0dqN-osUB5Iw7Q"
     alt="cica">
