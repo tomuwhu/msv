348a007eef7a51a58970bac6969c54a2977973ed
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-31 13:37:29 +0100

    Docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
                 <li><a target="_blank" href="https://kit.svelte.dev/docs/routing"> Basic Routing / Layout Menu</a></li>
                 <li><a target="_blank"  href="https://svelte.dev/docs/kit/load">Load</a>, <a href="https://svelte.dev/docs/kit/form-actions">Form actions</a></li>
                 <li><a target="_blank"  href="https://svelte.dev/docs/kit/state-management">State management</a></li>
+                <li><a target="_blank"  href="https://svelte.dev/tutorial/kit/post-handlers">Ajax API</a></li>
             </ul>
         </li>
         <li><a target="_blank"  href="https://orm.drizzle.team/docs/get-started/mysql-new">Drizzle (MySQL)</a>
