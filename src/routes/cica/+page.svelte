5a4c7a0623a5bcca93172d099519f7ac5a101fbc
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-01-31 10:17:27 +0100

    cica

--- src/routes/cica/+page.svelte
+++ src/routes/cica/+page.svelte
@@ -1,8 +1,27 @@
 <script>
     let { data } = $props();
 </script>
-{#if data.user}
-Bejelentkezve: {data.user?.username}
-{:else}
-Nincs bejelentkezve
-{/if}
+<h1>Cica
+    (<span>
+        {#if data.user}
+            ~ {data.user?.username} ~
+        {:else}
+            <span class="err"> &lt;&lt; Nincs bejelentkezve &gt;&gt;</span>
+        {/if}
+    </span>)
+</h1>
+
+<style>
+h1 {
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    margin-bottom: 5px;
+    margin-top: 0px;
+}
+span {
+    color: rgb(70, 159, 164);
+}
+span.err {
+    color: red;
+    font-size: 25px;
+}
+</style>
